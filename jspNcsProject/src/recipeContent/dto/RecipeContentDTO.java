670db4cc712564ada6b39af624cc274ef4fe7b21
Jiye Choi <hyde69ciel@gmail.com>
2020-07-14 12:16:30 +0900

    dto

---
+++ jspNcsProject/src/recipeContent/dto/RecipeContentDTO.java
@@ -0,0 +1,42 @@
+package recipeContent.dto;
+
+public class RecipeContentDTO {
+	
+	int num;
+	int recipe_num;
+	int step;
+	String img;
+	String content;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int getRecipe_num() {
+		return recipe_num;
+	}
+	public void setRecipe_num(int recipe_num) {
+		this.recipe_num = recipe_num;
+	}
+	public int getStep() {
+		return step;
+	}
+	public void setStep(int step) {
+		this.step = step;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
---
+++ jspNcsProject/src/recipeContentComment/dto/RecipeContentCommentDTO.java
@@ -0,0 +1,71 @@
+package recipeContentComment.dto;
+
+import java.sql.Timestamp;
+
+public class RecipeContentCommentDTO {
+	
+	int num;
+	int recipe_num;
+	int content_num;
+	int ref;
+	int re_level;
+	int re_step;
+	String content;
+	String name;
+	Timestamp reg;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int getRecipe_num() {
+		return recipe_num;
+	}
+	public void setRecipe_num(int recipe_num) {
+		this.recipe_num = recipe_num;
+	}
+	public int getContent_num() {
+		return content_num;
+	}
+	public void setContent_num(int content_num) {
+		this.content_num = content_num;
+	}
+	public int getRef() {
+		return ref;
+	}
+	public void setRef(int ref) {
+		this.ref = ref;
+	}
+	public int getRe_level() {
+		return re_level;
+	}
+	public void setRe_level(int re_level) {
+		this.re_level = re_level;
+	}
+	public int getRe_step() {
+		return re_step;
+	}
+	public void setRe_step(int re_step) {
+		this.re_step = re_step;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Timestamp getReg() {
+		return reg;
+	}
+	public void setReg(Timestamp reg) {
+		this.reg = reg;
+	}
+	
+}
