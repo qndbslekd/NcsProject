ee7a0870df173c04d46adc875564aeba1559e729
hotdog1004 <hotdog87675210@gmail.com>
2020-07-15 10:55:53 +0900

    레시피 내용 보기 (미완성)

---
+++ jspNcsProject/src/jspNcsProject/dto/RecipeCommentDTO.java
@@ -0,0 +1,65 @@
+package jspNcsProject.dto;
+
+import java.sql.Timestamp;
+
+public class RecipeCommentDTO {
+	int num;
+	int recipeNum;
+	int ref;
+	int re_level;
+	int re_step;
+	String content;
+	String name;
+	Timestamp reg;
+	
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int getRecipeNum() {
+		return recipeNum;
+	}
+	public void setRecipeNum(int recipeNum) {
+		this.recipeNum = recipeNum;
+	}
+	public int getRef() {
+		return ref;
+	}
+	public void setRef(int ref) {
+		this.ref = ref;
+	}
+	public int getRe_level() {
+		return re_level;
+	}
+	public void setRe_level(int re_level) {
+		this.re_level = re_level;
+	}
+	public int getRe_step() {
+		return re_step;
+	}
+	public void setRe_step(int re_step) {
+		this.re_step = re_step;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Timestamp getReg() {
+		return reg;
+	}
+	public void setReg(Timestamp reg) {
+		this.reg = reg;
+	}
+	
+}
