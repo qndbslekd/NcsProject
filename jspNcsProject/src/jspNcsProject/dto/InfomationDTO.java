dbd6b48d8a71c681b89eec7f56d1959d524f3006
g <g@DESKTOP-TP5LA2B>
2020-07-16 13:14:51 +0900

    ## im > in update information..

---
+++ jspNcsProject/src/jspNcsProject/dto/InfomationDTO.java
@@ -0,0 +1,22 @@
+package jspNcsProject.dto;
+
+public class InfomationDTO {
+	private String subject;
+	private String content;
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	@Override
+	public String toString() {
+		return "ImfomationDTO [subject=" + subject + ", content=" + content + "]";
+	}
+}
